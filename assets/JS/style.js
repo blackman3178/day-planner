905484b793dc914fe8a92f2b6152ea857208d441
Noah Blackman <nablackm@usc.edu>
2021-09-24 16:38:53 -0400

    Commit with the beginning of the makeTimeTable() fxn NEED TO FINISH

--- assets/JS/style.js
+++ assets/JS/style.js
@@ -4,3 +4,18 @@ console.log(currentDay);
 timeTextEl.text(currentDay);
 //^^ uses moment.js to display day & month at bottom of jumbotron.
 
+//generates the time table with blank inputs
+function makeTimeTable() {
+    var tableEl = $('.table-element');
+    for (var i = 8; i < 25; i++) {
+        var timeRowEl = $("<tr>").text([i] + ":00");
+        var todoRowEL = $("<tr>").addClass("col-10");
+        var saveEl = $("<tr>").addClass("bg-primary text-light position-absolute top-50 start-50 translate-middle");
+
+        tableEl.append(
+            timeRowEl,
+            todoRowEL,
+            saveEl
+        );
+    }
+}
